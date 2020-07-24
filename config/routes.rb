319357be318d3c3c1a5fba2904c3081d4c964e59
JuanPablo CastiglioniDupin <jpcastiglioni@gmail.com>
2020-07-24 20:06:37 -0300

    Working restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # root to: 'pages#home'
   root to: 'restaurants#index'
-  resources :restaurants, only: [ :index, :show ]
+  resources :restaurants, only: [ :index, :show ] do
+    resources :reviews, only: :create
+  end
 end
